6eec3d25078c6ce413a037e39ffaed81bd892274
Alkaar <alkaar@gmail.com>
2022-12-02 19:15:15 -0500

    Day 01 solution

--- src/day01/Main.hs
+++ src/day01/Main.hs
@@ -1,7 +1,20 @@
 module Main (main) where
 
+import qualified Data.List as List
+
 main :: IO ()
-main =
-  do
-    contents <- readFile "resources/day01.txt"
-    print contents
+main = do
+       contents <- readFile "resources/day01.txt"
+       let calories = foldl (\x y -> case y of
+                                     "" -> x ++ [0]
+                                     _ -> init x ++ [last x + (read y :: Int)]) [0] . lines $ contents
+
+       print $ maximum calories
+       print $ topCalories 3 calories
+
+topCalories :: (Num n, Ord n, Num l, Ord l) => n -> [l] -> l
+topCalories n l
+    | n == 0    = 0
+    | otherwise = do
+                  let maxNum = maximum l
+                  maxNum + topCalories (n-1) (List.delete maxNum l)
